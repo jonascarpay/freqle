e212116fe2a0dd864bd654164effd43d8d5754d4
Jonas Carpay <jonascarpay@gmail.com>
2024-01-02 12:15:29 +0900

    Command struct

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use clap::{arg, command, Command};
+use clap::{arg, command};
 
 fn main() {
     let matches = command!()
@@ -6,10 +6,44 @@ fn main() {
         .arg(arg!(-c --config <FILE> "Sets a config file").required(false))
         .arg(arg!(-d --debug ... "Turn debugging info on"))
         .subcommand(
-            Command::new("test")
+            clap::Command::new("test")
                 .about("tests")
                 .arg(arg!(-l --list "does list")),
         )
         .get_matches();
     print!("{matches:?}");
+}
+
+struct Weights {
+    hour: f64,
+    day: f64,
+    month: f64,
+}
+
+struct FileArgs {
+    path: String,
+    strict: bool,
+}
+
+enum Command {
+    Bump {
+        file_args: FileArgs,
+        query: String,
+        thresh: f64,
+    },
+    Touch {
+        file_args: FileArgs,
+        thresh: f64,
+    },
+    Delete {
+        file_args: FileArgs,
+        query: String,
+    },
+    View {
+        file_args: FileArgs,
+        weights: Weights,
+        augment: bool,
+        restrict: bool,
+        verbose: bool,
+    },
 }
